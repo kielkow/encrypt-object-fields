b1ab4914242d39b32c27adc79689dd4b1f5fad31
kielkow <matheuskiel@fiorifer.com.br>
2020-09-29 19:21:05 -0300

    Recursive decrypt function

--- decrypt-object.js
+++ decrypt-object.js
@@ -1,82 +1,21 @@
-const crypto = require('crypto');
-const { isEmpty } = require('lodash');
+const { createDecipher } = require('crypto');
 
-async function getDecryptedObject(payload, fields) {
-    if (typeof payload !== 'object' || isEmpty(payload)) return payload;
+module.exports = async (obj, fields) => {
+    const decrypt = (async obj => {
+        for (const prop in obj) {
+            if (Array.isArray(obj[prop]) || typeof obj[prop] === 'object') await decrypt(obj[prop]);
 
-    for (const field of fields) {
-        if (isEmpty(field)) continue;
+            if (obj[prop] === null || obj[prop] === '' || obj[prop] === undefined) continue;
 
-        if (typeof field === "object") {
-            const key = Object.keys(field)[0];
+            const decipher = createDecipher('aes256', 'encryptobjectfields');
 
-            eval(
-                `var decrypt = async function() {
-                    try {
-                        for (const element of payload.${key}) {
-                            const values = Object.keys(element);
-
-                            for (const value of values) {
-                                if (Object.values(field)[0].includes(value) && !isEmpty(element[value])) {
-                                    const criptoParams = {
-                                        algoritm: "aes256",
-                                        secret: "encryptobjectfields",
-                                    };
-
-                                    const decipher = await crypto.createDecipher(criptoParams.algoritm, criptoParams.secret);
-
-                                    let decrypted = decipher.update(element[value].toString(), 'hex', 'utf8');
-                
-                                    decrypted += decipher.final('utf8');
-
-                                    element[value] = decrypted;
-                                }
-                            }
-                        }
-                    }
-                    catch (err) {
-                        throw new Error(err.stack || err);
-                    }
-                }`
-            );
-
-            await decrypt();
+            if (fields.includes(prop)) obj[prop] = decipher.update(obj[prop], 'hex', 'utf8') + decipher.final('utf8');
         }
-        else {
-            eval(
-                `var decrypt = async function() {
-                    if (!isEmpty(payload.${field})) {
-                        try {
-                            const criptoParams = {
-                                algoritm: "aes256",
-                                secret: "encryptobjectfields",
-                            };
-        
-                            const decipher = await crypto.createDecipher(criptoParams.algoritm, criptoParams.secret);
 
-                            let decrypted = decipher.update(payload.${field}.toString(), 'hex', 'utf8');
-        
-                            decrypted += decipher.final('utf8');
-        
-                            payload.${field} = decrypted;
-                        }
-                        catch (err) {
-                            throw new Error(err.stack || err);
-                        }
-                    }
-                }`
-            );
-    
-            await decrypt();
-        }
-    }
+        return obj;
+    });
 
-    return payload;
+    const decrypted = await decrypt(JSON.parse(JSON.stringify(obj)));
+
+    return decrypted;
 };
-
-module.exports = async (payload, fields) => {
-    return getDecryptedObject(
-        JSON.parse(JSON.stringify(payload)), 
-        fields
-    );
-}
