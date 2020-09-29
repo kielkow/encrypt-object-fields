f7b02606e05c9557cb67b1898e15829a9560a5ab
kielkow <matheuskiel@fiorifer.com.br>
2020-09-29 19:08:18 -0300

    Recursive encrypt function

--- encrypt-object.js
+++ encrypt-object.js
@@ -1,73 +1,21 @@
-const { isEmpty } = require('lodash');
 const { createCipher } = require('crypto');
 
-async function getEncryptedObject(payload, fields) {
-    if (typeof payload !== 'object' || isEmpty(payload)) return payload;
+module.exports = async (obj, fields) => {
+    const crypt = (async obj => {
+        for (const prop in obj) {
+            if (Array.isArray(obj[prop]) || typeof obj[prop] === 'object') await crypt(obj[prop]);
 
-    for (const field of fields) {
-        if (!isEmpty(field)) {
-            if (typeof field === "object") {
-                const key = Object.keys(field)[0];
+            if (obj[prop] === null || obj[prop] === '' || obj[prop] === undefined) continue;
 
-                eval(
-                    `var encrypt = async function() {
-                        try {
-                            for (const element of payload.${key}) {
-                                const values = Object.keys(element);
+            const cipher = createCipher('aes256', 'encryptobjectfields');
 
-                                for (const value of values) {
-                                    if (Object.values(field)[0].includes(value) && !isEmpty(element[value])) {
-                                        const criptoParams = {
-                                            algoritm: "aes256",
-                                            secret: "encryptobjectfields",
-                                        };
+            if (fields.includes(prop)) obj[prop] = cipher.update(obj[prop], 'utf8', 'hex') + cipher.final('hex');
+        }
 
-                                        const cipher = await createCipher(criptoParams.algoritm, criptoParams.secret);
+        return obj;
+    });
 
-                                        const encrypted = cipher.update(element[value].toString(), 'utf8', 'hex') + cipher.final('hex');
+    const encrypted = await crypt(JSON.parse(JSON.stringify(obj)));
 
-                                        element[value] = encrypted;
-                                    }
-                                }
-                            }
-                        }
-                        catch (err) {
-                            throw new Error(err.stack || err);
-                        }
-                    }`
-                );
-
-                await encrypt();
-            }
-            else {
-                eval(
-                    `var encrypt = async function() {
-                        if (!isEmpty(payload.${field})) {
-                            try {
-                                const criptoParams = {
-                                    algoritm: "aes256",
-                                    secret: "encryptobjectfields",
-                                };
-            
-                                const cipher = await createCipher(criptoParams.algoritm, criptoParams.secret);
-            
-                                const encrypted = cipher.update(payload.${field}.toString(), 'utf8', 'hex') + cipher.final('hex');
-            
-                                payload.${field} = encrypted;
-                            }
-                            catch (err) {
-                                throw new Error(err.stack || err);
-                            }
-                        }
-                    }`
-                );
-
-                await encrypt();
-            }
-        };
-    }
-
-    return payload;
+    return encrypted;
 };
-
-module.exports = async (payload, fields) => getEncryptedObject(JSON.parse(JSON.stringify(payload)), fields);
